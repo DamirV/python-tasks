dee857032f55e1f00655b76c96e3258b4635b130
DamirV <32214748+DamirV@users.noreply.github.com>
2022-11-17 18:35:02 +0300

    Add files via upload
    The road to Y

--- validation_of_brackets/main.py
+++ validation_of_brackets/main.py
@@ -11,9 +11,9 @@ def is_correct_brackets_v1(brackets):
             stack.append("}")
         elif i == "[":
             stack.append("]")
-        elif len(stack) == 0 or i != stack[-1]:
-            return False
-        else:
+        elif i == ")" or i == "}" or i == "]":
+            if len(stack) == 0 or i != stack[-1]:
+                return False
             stack.pop()
 
     return len(stack) == 0
@@ -26,7 +26,7 @@ def is_correct_brackets_v2(brackets):
     for i in brackets:
         if i == "(" or i == "[" or i == "{":
             stack.append(brackets_dict[i])
-        else:
+        elif i == ")" or i == "]" or i == "}":
             if len(stack) == 0 or i != stack[-1]:
                 return False
             stack.pop()
@@ -35,15 +35,25 @@ def is_correct_brackets_v2(brackets):
 
 
 if __name__ == '__main__':
-    brackets_right1 = "[]{[({{}})[]]}"
+    brackets_right1 = "abc[]{[({{}})[]]}"
     brackets_right2 = ""
+    brackets_right3 = "abc"
+    brackets_right4 = "[(){}]abc"
+    brackets_right5 = "abc"
+    brackets_right6 = '(":-[];)'
+    brackets_right7 = 'print(f"Expect True: {is_correct_brackets_v2(brackets_right7)}")'
     brackets_wrong1 = "{[(]}"
     brackets_wrong2 = "{[)]}"
-    brackets_wrong3 = "}"
-    brackets_wrong4 = "("
+    brackets_wrong3 = "abc}"
+    brackets_wrong4 = "(abc"
 
     print(f"Expect True: {is_correct_brackets_v2(brackets_right1)}")
     print(f"Expect True: {is_correct_brackets_v2(brackets_right2)}")
+    print(f"Expect True: {is_correct_brackets_v2(brackets_right3)}")
+    print(f"Expect True: {is_correct_brackets_v2(brackets_right4)}")
+    print(f"Expect True: {is_correct_brackets_v2(brackets_right5)}")
+    print(f"Expect True: {is_correct_brackets_v2(brackets_right6)}")
+    print(f"Expect True: {is_correct_brackets_v2(brackets_right7)}")
     print(f"Expect False: {is_correct_brackets_v2(brackets_wrong1)}")
     print(f"Expect False: {is_correct_brackets_v2(brackets_wrong2)}")
     print(f"Expect False: {is_correct_brackets_v2(brackets_wrong3)}")
